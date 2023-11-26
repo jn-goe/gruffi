507707bc249849f6aa42e729ef033f7ced60f997
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-26 10:31:33 +0100

    dep declaration

--- dev/Create.the.gruffi.Package.R
+++ dev/Create.the.gruffi.Package.R
@@ -28,7 +28,6 @@ DESCRIPTION <- list("Title" = "Gruffi identifies and removes stressed cells from
                       person(given = "Julia", family = "Naas", email = "julia.naas@meduniwien.ac.at", role =  c("aut", "cre") ),
                       person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )
                     )
-                    # , "Authors@R" = 'person(given = "Julia", family = "Naas", email = "julia.naas@meduniwien.ac.at", role =  c("aut", "cre") ), person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )'
                     , "Description" = "The Gruffi R package helps you (1) to identify stressed cells in single-cell
     RNA-seq datasets using  *granular funcitonal filtering*, and (2) you can use it to calculate any
     GO-term defined gene set's pathway activity. Gruffi integrates into single-cell analysis with
@@ -38,11 +37,12 @@ DESCRIPTION <- list("Title" = "Gruffi identifies and removes stressed cells from
                     , "Packaged" =  Sys.time()
                     , "Depends" =  "Seurat, magrittr, Stringendo, MarkdownReports"
                     , "Imports" = "cowplot, dplyr, ggplot2, raster, DOSE
-                    , MarkdownHelpers, CodeAndRoll2, Seurat.utils, ggExpress, stringr, sm, AnnotationDbi
+                    , MarkdownHelpers (>= 1.0.1), CodeAndRoll2, Seurat.utils, ggExpress, stringr, sm, AnnotationDbi
                     , IRanges, Matrix, biomaRt, clipr, htmlwidgets, org.Hs.eg.db, rgl, rlang, shiny, tictoc, viridis"
                     # , "Suggests" = ""
                     , "BugReports"= "https://github.com/jn-goe/gruffi/issues/"
 )
+
 
 
 setwd(RepositoryDir)
