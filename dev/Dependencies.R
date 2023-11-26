af14c0ed11be3b678c21d301f26f3043b1b0a996
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-26 01:52:35 +0100

    Deps

---
+++ dev/Dependencies.R
@@ -0,0 +1,143 @@
+
+$`character(0)`
+ [1] "AddGOScore"
+ [2] "binning"
+ [3] "calc.cluster.averages"
+ [4] "calc.cluster.averages.gruffi"
+ [5] "cividis"
+ [6] "clUMAP"
+ [7] "cube3d"
+ [8] "FeaturePlotSaveGO"
+ [9] "fix.metad.Colname.rm.trailing.1"
+[10] "geneInCategory"
+[11] "getBM"
+[12] "GetClusteringRuns"
+[13] "GetGOTerms"
+[14] "group_by_at"
+[15] "IntersectWithExpressed"
+[16] "isave.RDS"
+[17] "labs"
+[18] "modal"
+[19] "multiFeaturePlot.A4"
+[20] "n"
+[21] "open3d"
+[22] "PlotGoTermScores"
+[23] "PlotNormAndSkew"
+[24] "qbarplot"
+[25] "qhistogram"
+[26] "qqSave"
+[27] "qscatter"
+[28] "read_clip"
+[29] "rglwidget"
+[30] "runApp"
+[31] "save_plot"
+[32] "saveWidget"
+[33] "say"
+[34] "select"
+[35] "select_at"
+[36] "shade3d"
+[37] "shinyApp"
+[38] "sort.natural"
+[39] "stand_dev_skewed"
+[40] "summarize"
+[41] "sym"
+[42] "Term"
+[43] "tibble"
+[44] "tic"
+[45] "toc"
+[46] "translate3d"
+[47] "useEnsembl"
+[48] "ww.convert.GO_term.2.score"
+[49] "ww.convert.score.2.GO_term"
+
+$`package:base`
+ [1] "abs"          "all"
+ [3] "as.character" "as.factor"
+ [5] "as.logical"   "as.matrix"
+ [7] "as.numeric"   "by"
+ [9] "c"            "cat"
+[11] "cbind"        "ceiling"
+[13] "character"    "colnames"
+[15] "crossprod"    "data.frame"
+[17] "diag"         "dim"
+[19] "dput"         "exists"
+[21] "factor"       "file.path"
+[23] "floor"        "grepl"
+[25] "gsub"         "identical"
+[27] "is.list"      "is.null"
+[29] "length"       "levels"
+[31] "list"         "logical"
+[33] "make.names"   "matrix"
+[35] "max"          "mean"
+[37] "min"          "names"
+[39] "ncol"         "Negate"
+[41] "new.env"      "nrow"
+[43] "paste"        "paste0"
+[45] "print"        "range"
+[47] "rep"          "return"
+[49] "round"        "rownames"
+[51] "sample"       "saveRDS"
+[53] "scale"        "setdiff"
+[55] "signif"       "sort"
+[57] "source"       "sqrt"
+[59] "stop"         "stopifnot"
+[61] "subset"       "substitute"
+[63] "sum"          "system"
+[65] "table"        "tcrossprod"
+[67] "try"          "unique"
+[69] "unlist"       "which"
+[71] "which.min"
+
+$`package:CodeAndRoll2`
+[1] "cv"
+[2] "grepv"
+[3] "matrix.fromNames"
+[4] "pc_TRUE"
+[5] "sem"
+[6] "sortbyitsnames"
+[7] "which_names"
+
+$`package:graphics`
+[1] "hist"
+
+$`package:MarkdownHelpers`
+[1] "filter_HP" "llprint"
+[3] "stopif"
+
+$`package:Seurat`
+[1] "AddModuleScore" "FeaturePlot"
+[3] "FindClusters"   "RunTSNE"
+[5] "RunUMAP"
+
+$`package:stats`
+[1] "dist"     "median"   "qnorm"
+[4] "quantile"
+
+$`package:Stringendo`
+[1] "flag.nameiftrue"
+[2] "iprint"
+[3] "kpp"
+[4] "kppd"
+[5] "percentage_formatter"
+[6] "ppp"
+[7] "ww.FnP_parser"
+
+$`package:stringr`
+[1] "str_split_fixed"
+
+$`package:utils`
+[1] "head"
+
+c("devtools_shims", "base")
+c("Seurat", "SeuratObject")
+c("SeuratObject", "base")
+character(0)
+base
+CodeAndRoll2
+graphics
+MarkdownHelpers
+Seurat
+stats
+Stringendo
+stringr
+utils
